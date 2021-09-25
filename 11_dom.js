ad7a15dd61c5d81cfc17d8b4b180eb705dcbe089
Poulami Mandal <poulami.kv@gmail.com>
2021-09-26 05:16:03 +0530

    5th commit

---
+++ 11_dom.js
@@ -0,0 +1,13 @@
+let a = document;
+a= document.body;
+a=document.forms;
+Array.from(a).forEach(function(element){
+    console.log(element);
+ })
+a=document.links[0].href;
+a=document.links[0];
+ let b= document.images.links;
+ let c =document.scripts;
+console.log(a);
+console.log(b);
+console.log(c);
